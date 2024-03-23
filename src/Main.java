2c901096fae0136da16a2caac269b607e3c2e0f4
Mehmet Fatih Balkı <159441317+mefatihbalki@users.noreply.github.com>
2024-03-23 03:46:46 +0300

    yorum satırları

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,20 @@ public class Main {
         int index;
         Scanner input = new Scanner(System.in);
 
+        //veri alınan kısım
         System.out.print("Dizideki indeks : ");
         index = input.nextInt();
         indexException(arr,index);
     }
 
+    //kullanılan try catch bölümü
     public static void indexException (int[] arr, int index) {
         try {
+
+            //başarılı ise seçilen indexin yazıldı kısım
             System.out.println("Seçilen index : " + arr[index] );
+
+            //hata mesajı
         }catch (ArrayIndexOutOfBoundsException e){
             System.out.println(" Hata : " +e.getMessage() + " ArrayIndexOutOfBoundsException");
         }
